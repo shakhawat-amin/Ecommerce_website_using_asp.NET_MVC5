21197271625a4fba458b00f5a7b774297eb3e632
shakhawat-amin <s.shakhawat.amin@gmail.com>
2016-10-03 01:01:56 +0600

    3 oct 16
    first upload in git. prottekta version upload korte hobe ekhon theke

---
+++ Ecommerce version2/Ecommerce version2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ecommerce_version2.MvcApplication" Language="C#" %>
---
+++ Ecommerce version2/Ecommerce version2/Models/AdminForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce_version2.Models
+{
+    public class AdminForm
+    {
+        public string adminName {get; set;}
+        public string password {get; set;}
+    }
+}
